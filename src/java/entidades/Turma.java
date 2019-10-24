4f91ec1945cbe8207d90717f14bfad492532ddd3
Dudu brabo <Aluno@C302-LAB04-007>
2019-10-24 21:04:17 -0200

    Turma Adiocionada

---
+++ src/java/entidades/Turma.java
@@ -0,0 +1,90 @@
+
+package entidades;
+
+public class Turma {
+    
+    private int id;
+    private String nome;
+    private String sala;
+    private int ano;
+    private int idTurno;
+    private int idAluno;
+    private int idProf;
+    private int idCurso;
+    private int idUc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getIdTurno() {
+        return idTurno;
+    }
+
+    public void setIdTurno(int idTurno) {
+        this.idTurno = idTurno;
+    }
+
+    public int getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(int idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public int getIdProf() {
+        return idProf;
+    }
+
+    public void setIdProf(int idProf) {
+        this.idProf = idProf;
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public int getIdUc() {
+        return idUc;
+    }
+
+    public void setIdUc(int idUc) {
+        this.idUc = idUc;
+    }
+    
+    
+    
+}
