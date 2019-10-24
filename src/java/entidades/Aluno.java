c03198f7b30f74b3e70adb44536ac9e8f91fb28a
Aluno <Aluno@C302-LAB04-008>
2019-10-24 21:09:54 -0200

    Att de Aluno Incrementação de Unidadec

--- src/java/entidades/Aluno.java
+++ src/java/entidades/Aluno.java
@@ -15,5 +15,45 @@ public class Aluno {
     private String email;
     private String cpf;
     private int fone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getFone() {
+        return fone;
+    }
+
+    public void setFone(int fone) {
+        this.fone = fone;
+    }
   
 }
---
+++ src/java/entidades/Unidadec.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Unidadec {
+    private int id;
+    private String descricao;
+    private int cargahoraria;
+    private int idNota;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargahoraria() {
+        return cargahoraria;
+    }
+
+    public void setCargahoraria(int cargahoraria) {
+        this.cargahoraria = cargahoraria;
+    }
+
+    public int getIdNota() {
+        return idNota;
+    }
+
+    public void setIdNota(int idNota) {
+        this.idNota = idNota;
+    }
+}
