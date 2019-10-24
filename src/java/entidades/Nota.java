b840cf05fae70d19aaee77e58ae94ad73c8fd924
Dudu brabo <>
2019-10-24 20:44:35 -0200

    Adiconado nota e professor

---
+++ src/java/entidades/Nota.java
@@ -0,0 +1,78 @@
+
+package entidades;
+
+
+public class Nota {
+    private int id;
+    private Double n1;
+    private Double n2;
+    private Double n3;
+    private Double n4;
+    private String stts;
+    private Double media;
+    private Double nota_da_recuperacao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getN1() {
+        return n1;
+    }
+
+    public void setN1(Double n1) {
+        this.n1 = n1;
+    }
+
+    public Double getN2() {
+        return n2;
+    }
+
+    public void setN2(Double n2) {
+        this.n2 = n2;
+    }
+
+    public Double getN3() {
+        return n3;
+    }
+
+    public void setN3(Double n3) {
+        this.n3 = n3;
+    }
+
+    public Double getN4() {
+        return n4;
+    }
+
+    public void setN4(Double n4) {
+        this.n4 = n4;
+    }
+
+    public String getStts() {
+        return stts;
+    }
+
+    public void setStts(String stts) {
+        this.stts = stts;
+    }
+
+    public Double getMedia() {
+        return media;
+    }
+
+    public void setMedia(Double media) {
+        this.media = media;
+    }
+
+    public Double getNota_da_recuperacao() {
+        return nota_da_recuperacao;
+    }
+
+    public void setNota_da_recuperacao(Double nota_da_recuperacao) {
+        this.nota_da_recuperacao = nota_da_recuperacao;
+    }
+}
---
+++ src/java/entidades/Professor.java
@@ -0,0 +1,52 @@
+
+package entidades;
+
+
+public class Professor {
+    private int id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private int fone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getFone() {
+        return fone;
+    }
+
+    public void setFone(int fone) {
+        this.fone = fone;
+    }
+    
+}
